0c17c8266b5709061d982c8f3c039e009b14c028
Murgennnn <murgennnn@gmail.com>
2024-12-08 22:45:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 'use strict';
-const express = require('express');
-const app = express();
-const http = require('http');
-const _server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(_server);
-const fs = require("fs");
-
-//const ServFunc = require('./scripting/server.js');
-global.server = new ServFunc(io);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); //index-svg.html
+const { createServer } = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
-
-io.on('connection', async (socket) => {
-  console.log("CONECT: " + socket.id);
-
-  //await server.addPlayer('1982352306', socket);
-
-  socket.on("disconnect", async () => {
-      //await server.disconectPlayer('1982352306');
-  });
-});
-
-_server.listen(3000, async () => {
-    console.log('listening on *:3000');
-    server.addSpawn();
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
