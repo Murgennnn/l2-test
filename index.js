b057f87b8e0b5f9f15061333804edb7007784085
Murgennnn <murgennnn@gmail.com>
2024-12-09 00:32:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-'use strict';
 const express = require('express');
-const app = express();
-const http = require('http');
-const _server = http.createServer(app);
-const { Server } = require("socket.io");
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+
+// Настройка маршрутов и middleware
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.send('Hello, HTTPS!');
 });
 
-_server.listen(3000, async () => {
-    console.log('listening on *:8080');
+// Путь к вашим SSL-сертификатам и ключам
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/v2680742.hosted-by-vdsina.ru/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/v2680742.hosted-by-vdsina.ru/fullchain.pem')
+};
+
+// Создание HTTPS сервера
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS Server running on port 443');
 });
