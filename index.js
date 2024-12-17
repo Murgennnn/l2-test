2b5a87a64f19270362f7334c011c03a4f3bf659c
Murgennnn <murgennnn@gmail.com>
2024-12-17 23:28:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 const app = express();
 
+const TelegramBot = require('node-telegram-bot-api');
+const token = '7519170735:AAFKuhy-oaNX2tIDvGSi5Tko1MzkIrPHZZw';
+const bot = new TelegramBot(token, { polling: true });
+
 // Настройка маршрутов и middleware
 app.get('/', (req, res) => {
   res.send('Hello, HTTPS!');
@@ -20,3 +24,21 @@ const options = {
 https.createServer(options, app).listen(443, () => {
   console.log('HTTPS Server running on port 443');
 });
+
+bot.on('message', async (msg) => { 
+	const chatId = msg.chat.id;
+	var _text = msg.text;
+	
+	try{
+		bot.sendMessage(
+			chatId, 
+			_text, 
+			{
+				reply_markup: {
+					keyboard: JSON.parse('[]'),
+					resize_keyboard: true	
+				},parse_mode: 'Markdown'			
+				}
+		);
+	}catch(e){ console.log(e) }
+}
